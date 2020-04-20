218c6f4d9bf8a79b0ec336104a13e2bba9ef49f6
ryderfwj <63780516+ryderfwj@users.noreply.github.com>
2020-04-20 18:31:44 +0800

    Update 57

---
+++ javascript/pages.js
@@ -0,0 +1,17 @@
+function warningOverlay(){
+  
+}
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+} 
+
+$(document).ready(function(){
+  $("#overlay").click(function(){
+    off();
+  });
+});
