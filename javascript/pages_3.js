dbcdacaf10d90a8367c3422a9505e410baffed47
ryderfwj <63780516+ryderfwj@users.noreply.github.com>
2020-07-25 18:44:59 +0800

    C65 done

--- javascript/pages_3.js
+++ javascript/pages_3.js
@@ -1,3 +1,7 @@
+var audioTrack1 = document.getElementById("audioControl1");
+var audioTrack2 = document.getElementById("audioControl2");
+var oddEven = 1;
+
 function on() {
   document.getElementById("warnOverlay").style.display = "block";
 }
@@ -6,8 +10,65 @@ function off() {
   document.getElementById("warnOverlay").style.display = "none";
 }
 
+function audioVolume() {
+  audioTrack1.volume = 0.50;
+  audioTrack2.volume = 0.25;
+}
+
+function pauseAudio() {
+  if (oddEven & 1) {
+    audioTrack1.pause();
+  } else {
+    audioTrack2.pause();
+  }
+}
+
+function pauseBothAudio() {
+  audioTrack2.pause();
+  audioTrack1.pause();
+}
+
+function resumeAudio() {
+  if (oddEven & 1) {
+    audioTrack1.play();
+  } else {
+    audioTrack2.play();
+  }
+}
+
 $(document).ready(function () {
+  pauseBothAudio();
+
+  audioVolume();
+
   $("#warnOverlay").click(function () {
     off();
+    resumeAudio();
+  });
+
+  $("#audioSlider").click(function () {
+    if (oddEven & 1) {
+      if (audioTrack1.paused) {
+        resumeAudio();
+      } else {
+        pauseAudio();
+      }
+    } else {
+      if (audioTrack2.paused) {
+        resumeAudio();
+      } else {
+        pauseAudio();
+      }
+    }
+  });
+
+  audioTrack1.addEventListener('ended', function () {
+    audioTrack2.play();
+    oddEven++;
+  });
+
+  audioTrack2.addEventListener('ended', function () {
+    audioTrack1.play();
+    oddEven++;
   });
 });
